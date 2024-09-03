603f6d8e94f1ff6a9477acc445fd77e3e27d0c06
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-09-03 19:49:50 -0300

    atividade 1, 2 e 3

---
+++ ListaDeExercicios/at001.py
@@ -0,0 +1,2 @@
+numero = float(input("Digite um número: "))
+print("O número informado foi {}.".format(numero))
---
+++ ListaDeExercicios/at003.py
@@ -0,0 +1,7 @@
+n1 = float(input("Primeira nota: "))
+n2 = float(input("Segunda nota: "))
+n3 = float(input("Terceira nota: "))
+n4 = float(input("Quarta nota: "))
+media = (n1 + n2 + n3 + n4) / 4
+
+print("A media do aluno(a) é {:.1f}.".format(media))
