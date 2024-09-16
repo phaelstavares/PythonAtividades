53643462b9e58a6afe303d37bc6b5fb07fcd1ad1
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-09-16 13:24:43 -0300

    atividade 7

---
+++ ListaDeExercicios/at007.py
@@ -0,0 +1,4 @@
+ganho = float(input("Quanto você ganha por hora: "))
+horas = float(input("Número de horas trabalhadas no mês: "))
+total = ganho * horas
+input("O total do seu salário no mês é de {}".format(total))
