589aa8d15242e694ca0fb5298ec879232b2336bd
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-09-16 13:11:34 -0300

    atividade 6

---
+++ ListaDeExercicios/at006.py
@@ -0,0 +1,5 @@
+quadrado = float(input("Digite o valor do quadrado: "))
+area = quadrado ** 2
+dobro = area * 2
+print("A área do quadrado é: {}".format(area))
+print("O dobro da área do quadrado é: {}".format(dobro))
